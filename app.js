4dd781b0d9cc1bca1ac884f12ecd8b11af4e232a
Zilola <44432264+zilola08@users.noreply.github.com>
2025-02-13 18:50:09 +0500

    Update app.js

--- app.js
+++ app.js
@@ -187,8 +187,8 @@ function createBoard(gameSize) {
       gameboard.style.width = `${window.innerWidth * 0.8}px`;
       gameboard.style.height = `${window.innerWidth * 0.8}px`;
     } else if (window.innerWidth >= 500 && window.innerWidth < 1000) {
-      gameboard.style.width = `${window.innerWidth * 0.5}px`;
-      gameboard.style.height = `${window.innerWidth * 0.5}px`;
+      gameboard.style.width = `${window.innerWidth * 0.4}px`;
+      gameboard.style.height = `${window.innerWidth * 0.4}px`;
     } else if (window.innerWidth >= 1000 && window.innerWidth < 1500) {
       gameboard.style.width = `${window.innerWidth * 0.4}px`;
       gameboard.style.height = `${window.innerWidth * 0.4}px`;
